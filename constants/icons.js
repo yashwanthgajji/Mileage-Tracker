de74c56aaf00c3f8ceeeedcdf80dc9de092bf490
Yashwanth Gajji <gajjiyashwanth7@gmail.com>
2024-08-16 21:57:43 -0400

    adding icons and fonts

---
+++ constants/icons.js
@@ -0,0 +1,15 @@
+import avatar from '../assets/icons/avatar.png'
+import car from '../assets/icons/car.png'
+import fuel from '../assets/icons/fuel.png'
+import home from '../assets/icons/home.png'
+import performance from '../assets/icons/performance.png'
+import plus from '../assets/icons/plus.png'
+
+export default {
+  avatar,
+  car,
+  fuel,
+  home,
+  performance,
+  plus
+};
