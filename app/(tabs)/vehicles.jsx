3fb20685ab989fa77771e54421c88c3e3e91acc3
Yashwanth Gajji <gajjiyashwanth7@gmail.com>
2024-08-20 18:10:18 -0400

    vehicles page

--- app/(tabs)/vehicles.jsx
+++ app/(tabs)/vehicles.jsx
@@ -1,11 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, FlatList } from 'react-native'
+import React, { useState } from 'react'
+import VehicleCard from '../../components/VehicleCard'
 
 const Vehicles = () => {
+  const vehicles = [{id: 1}, {id: 2}, {id: 3}]
   return (
-    <View>
-      <Text>Vehicles</Text>
+    <SafeAreaView className="bg-background h-full">
+      <View className="w-full justify-center items-center px-5 my-10">
+        <Text className="text-2xl text-start w-full mt-7 text-primary-800 font-psemibold">Vehicles</Text>
+        <FlatList
+          className="mt-7"
+          data={vehicles}
+          keyExtractor={(item) => {item.$id}}
+          renderItem={({item}) => (
+              <View className="w-full">
+                <VehicleCard />
+              </View>
+          )}
+        />
     </View>
+    </SafeAreaView>
   )
 }
 
--- components/VehicleCard.jsx
+++ components/VehicleCard.jsx
@@ -3,30 +3,30 @@ import React from 'react'
 import { images } from '../constants'
 
 const VehicleCard = ({ containerStyles }) => {
-    const hasVehicleImage = false
+    const hasVehicleImage = true
     return (
-    <View className="w-80 h-56 rounded-2xl mt-3 justify-start items-center bg-orange-100 flex-col">
+    <View className="w-[356px] h-60 rounded-2xl my-3 justify-start items-center bg-orange-100 flex-col">
         <View className="w-full flex-1">
         { 
             hasVehicleImage ? (
-                <View className="flex-col w-full h-full justify-center items-center bg-blue-50 rounded-2xl">
-                    <Image 
-                        source={images.bike}
-                        className="h-[50%] w-[50%]"
-                        resizeMode='contain'
-                    />
-                    <Text className="text-black-200 text-xl font-pmedium">No Image</Text>
-                </View>
-            ) : (
                 <Image 
                     source={images.royalenfield} 
                     className="h-full w-full rounded-2xl"
                     resizeMode='cover'
                 />
+            ) : (
+                <View className="flex-col w-full h-full justify-center items-center bg-blue-50 rounded-2xl">
+                    <Image 
+                        source={images.car}
+                        className="h-[50%] w-[50%]"
+                        resizeMode='contain'
+                    />
+                    <Text className="text-black-200 text-xl font-pmedium">No Image</Text>
+                </View>  
             )
         }
         </View>
-        <View className="w-full flex-row items-center justify-start m-2 px-4 space-x-1">
+        <View className="w-full flex-row items-center justify-start m-2 px-6 space-x-1">
             <View className="w-full flex-col items-start justify-center space-y-1 flex-1">
                 <Text className="text-primary-800 text-base font-pregular">Royal Enfield</Text>
                 <Text className="text-black-200 text-xs font-pmedium">2 Wheeler</Text>
